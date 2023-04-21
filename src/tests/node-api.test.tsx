9b5dd671b38cecfe9bf49f4df2a5567944fedb84
clangenb <37865735+clangenb@users.noreply.github.com>
2023-04-21 14:02:40 +0200

    bump encointer deps and fix integration tests (#14)
    * add yarn upgrade-interactive
    
    * upgrade encointer-deps
    
    * update tests
    
    * fix test assert
    
    * clean up test code

--- src/tests/node-api.test.tsx
+++ src/tests/node-api.test.tsx
@@ -1,11 +1,13 @@
 import {ApiPromise, Keyring, WsProvider} from '@polkadot/api';
 import {options} from "@encointer/node-api/options";
 import { communityIdentifierFromString } from "@encointer/util/cidUtil.js";
+import { CommunityIdentifier } from '@encointer/types';
 
 describe('node-api', () => {
   let keyring: Keyring;
   let api: any;
   const chain = 'ws://127.0.0.1:9944';
+  let testCid: CommunityIdentifier;
   beforeAll(async () => {
     // jest.setTimeout(90000);
     keyring = new Keyring({ type: 'sr25519' });
@@ -20,6 +22,8 @@ describe('node-api', () => {
       console.log(`connect ${chain} failed`);
       await provider.disconnect();
     }
+
+    testCid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G");
   }, 40000);
   afterAll(async () => {
     // Jest fails to exit after the tests without this.
@@ -28,50 +32,39 @@ describe('node-api', () => {
 
   describe('rpc', () => {
     it('communities.GetAll should return empty vec', async () => {
-      const result = await api.rpc.communities.getAll();
-      // console.log(result);
+      // @ts-ignore
+      const result = await api.rpc.encointer.getAllCommunities();
       expect(result.length).toBe(0);
     });
     it('communities.getLocations should return error on unknown community', async () => {
-      let cid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G");
-
       try {
         // @ts-ignore
-        await api.rpc.communities.getLocations(cid);
+        await api.rpc.encointer.getLocations(testCid);
       } catch (e) {
-        expect(e.toString()).toBe("Error: 3: Offchain storage not found: Key [99, 105, 100, 115, 103, 98, 115, 117, 118, 255, 255, 255, 255]");
+        expect(e.toString()).toBe("RpcError: 3: Offchain storage not found: [99, 105, 100, 115, 103, 98, 115, 117, 118, 255, 255, 255, 255]");
       }
     });
   });
   describe('bazaar', () => {
     it('bazaar.GetBusinesses should return empty vec', async () => {
-      const cid = api.createType('CommunityIdentifier', {
-        geohash: [0x00, 0x00, 0x00, 0x00, 0x00],
-        digest: [0x00, 0x00, 0x00, 0x00]
-      }); // @ts-ignore
-
-      const result = await api.rpc.bazaar.getBusinesses(cid.toHex()); // console.log(result);
-
+      // @ts-ignore
+      const result = await api.rpc.encointer.bazaarGetBusinesses(testCid.toHex());
       expect(result.length).toBe(0);
     });
     it('bazaar.GetOfferings should return empty vec', async () => {
-      // random cid
-      let cid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G"); // @ts-ignore
-
-      const result = await api.rpc.bazaar.getOfferings(cid); // console.log(result);
-
+      // @ts-ignore
+      const result = await api.rpc.encointer.bazaarGetOfferings(testCid);
       expect(result.length).toBe(0);
     });
     it('bazaar.GetOfferingsForBusiness should return empty vec', async () => {
-      // random cid
-      let cid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G");
       const alice = keyring.addFromUri('//Alice', {
         name: 'Alice default'
       });
-      const bid = api.createType('BusinessIdentifier', [cid, alice.publicKey]); // @ts-ignore
 
-      const result = await api.rpc.bazaar.getOfferingsForBusiness(bid); // console.log(result);
+      const bid = api.createType('BusinessIdentifier', [testCid, alice.publicKey]);
 
+      // @ts-ignore
+      const result = await api.rpc.encointer.bazaarGetOfferingsForBusiness(bid);
       expect(result.length).toBe(0);
     });
   });
