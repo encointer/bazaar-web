38e9f66bf694388c9c98a631f7780324c192fa5e
pifragile <pigu@bluewin.ch>
2023-08-02 11:24:57 +0200

    Bump deps to polkadot-v0.9.42 (#20)
    * bump deps
    
    * fix until BusinessData vs Business is consistent

--- src/BusinessComponent.tsx
+++ src/BusinessComponent.tsx
@@ -2,16 +2,17 @@ import { Business } from "./Types";
 import { resolveIpfs } from "./ipfs";
 
 export function BusinessComponent(props: { business: Business }) {
+    console.log(props.business)
     return (
         <ul>
             <li>
-                Name: {props.business.name}
-                <p> Description: {props.business.description} </p>
+                Name: {props.business?.name}
+                <p> Description: {props.business?.description} </p>
             </li>
             <img
                 alt="business icon"
                 style= {{width: "30vw"}}
-                src={resolveIpfs(props.business["logo"])}
+                src={resolveIpfs(props.business?.["logo"])}
             />
         </ul>
     );
