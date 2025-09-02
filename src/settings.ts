f480a243b634813910bb5a712502b3024557ecdf
Alain Brenzikofer <alain@integritee.network>
2025-09-02 13:53:36 +0200

    cleanup

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,6 @@
-export const IPFS_GATEWAY = 'https://encointer.infura-ipfs.io/ipfs'
+// export const IPFS_GATEWAY = 'https://encointer.infura-ipfs.io/ipfs'
+export const IPFS_GATEWAY = 'http://ipfs.encointer.org:8080/ipfs'
 export const localChain = 'ws://127.0.0.1:9944';
 
-export const remoteChain = 'wss://gesell.encointer.org';
+// export const remoteChain = 'wss://gesell.encointer.org';
+export const remoteChain = 'wss://kusama.api.encointer.org';
