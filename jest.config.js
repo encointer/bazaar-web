2cd7707592a61b5e6d16b285493153a332d9a79b
clangenb <37865735+clangenb@users.noreply.github.com>
2025-09-21 08:48:13 +0200

    test suite starts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,38 @@
+//const config = require('@polkadot/dev/config/jest.cjs');
+
+// Copyright 2017-2022 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+const { defaults } = require("jest-config");
+
 module.exports = {
-  testEnvironment: "jsdom",
-  transformIgnorePatterns: [
-    "node_modules/(?!(?:@polkadot|@encointer)/)"
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/build",
+    "<rootDir>/build-tsc-cjs",
+    "<rootDir>/build-tsc-ems",
+    "<rootDir>/build-tsc-js",
   ],
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  // See https://jestjs.io/docs/configuration#extraglobals-arraystring
+  sandboxInjectedGlobals: ["Math"],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+  ],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "node_modules/variables/.+\\.(j|t)sx?$": [
+      "ts-jest",
+      { tsconfig: "./tsconfig.jest.json", useESM: true },
+    ],
+    "\\.[jt]sx?$": [
+      "ts-jest",
+      { tsconfig: "./tsconfig.jest.json", useESM: true },
+    ],
+    ".ts": ["ts-jest", { tsconfig: "./tsconfig.jest.json", useESM: true }],
+  },
+  transformIgnorePatterns: ["node_modules/(?!variables/.*)"],
+  extensionsToTreatAsEsm: [".ts"],
+  resolver: "jest-ts-webcompat-resolver",
 };
