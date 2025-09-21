91a7890efc4faf8d8fd44ed732635053038e1315
clangenb <37865735+clangenb@users.noreply.github.com>
2025-09-21 08:32:30 +0200

    try fix integration tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { TextDecoder } from "@polkadot/x-textdecoder";
+import { TextEncoder } from "@polkadot/x-textencoder";
+
+// Polyfill for jsdom environment
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
