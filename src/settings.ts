87c0298c7a5530e849aaaa1e9700bab156612b42
Alain Brenzikofer <alain@integritee.network>
2025-09-23 15:49:49 +0200

    use ipfs gateway with https

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 // export const IPFS_GATEWAY = 'https://encointer.infura-ipfs.io/ipfs'
-export const IPFS_GATEWAY = 'http://ipfs.encointer.org:8080/ipfs'
+export const IPFS_GATEWAY = 'https://ipfs.integritee.network/ipfs'
 export const localChain = 'ws://127.0.0.1:9944';
 
 // export const remoteChain = 'wss://gesell.encointer.org';
